dd807761423406e5dd7cd79135944dcc09b15884
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-10-04 20:54:27 +0300

    create get route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { connectToDb, getDb } = require("./db");
 
 const PORT = 3001;
-const collectionName = "products";
+const COLLECTION = "products";
 const app = express();
 
 let db;
@@ -13,11 +13,31 @@ connectToDb((err) => {
       err
         ? console.log("err", err)
         : console.log(
-            `\n\nserver started on port: ${PORT} \n\nfull path to database: \nhttp://localhost:${PORT}/${collectionName}`
+            `\n\n
+            server started on port: ${PORT} 
+            \n\n
+            full path to database: 
+            \n
+            http://localhost:${PORT}/${COLLECTION}`
           );
     });
     db = getDb();
   } else {
     console.log(`Mongo db connection error: ${err}`);
   }
+});
+
+// Роут принимает два аргумента. Путь к коллекции и колбек
+app.get("/" + COLLECTION, (req, res) => {
+  const productsData = [];
+  db.collection(COLLECTION)
+    .find() // cursor - has next, forEach;
+    .forEach((product) => productsData.push(product))
+    .then(() => {
+      res.status(200).json(productsData);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something wrong" });
+      console.log("err", err);
+    });
 });
