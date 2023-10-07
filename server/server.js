71500575e2725c9014fc78dac63f6833c56413a9
vladimirSEOadv <vladimirSEOadv@gmail.com>
2023-10-07 19:56:44 +0300

    create delete route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
+const { json } = require("express");
 
 const PORT = 3001;
 const COLLECTION = "products";
 const app = express();
+app.use(express.json()); // Мидлвар позволяющий читать данные из body запроса к серверу
 
 let db;
 
@@ -13,12 +16,10 @@ connectToDb((err) => {
       err
         ? console.log("err", err)
         : console.log(
-            `\n\n
-            server started on port: ${PORT} 
-            \n\n
-            full path to database: 
-            \n
-            http://localhost:${PORT}/${COLLECTION}`
+            `
+Server started on port: ${PORT} 
+Full path to database:
+http://localhost:${PORT}/${COLLECTION}`
           );
     });
     db = getDb();
@@ -27,8 +28,12 @@ connectToDb((err) => {
   }
 });
 
+const handleError = (res, error) => {
+  res.status(500).json({ error });
+};
+
 // Роут принимает два аргумента. Путь к коллекции и колбек
-app.get("/" + COLLECTION, (req, res) => {
+app.get(`/${COLLECTION}`, (req, res) => {
   const productsData = [];
   db.collection(COLLECTION)
     .find() // cursor - has next, forEach;
@@ -36,8 +41,33 @@ app.get("/" + COLLECTION, (req, res) => {
     .then(() => {
       res.status(200).json(productsData);
     })
-    .catch((err) => {
-      res.status(500).json({ error: "Something wrong" });
-      console.log("err", err);
-    });
+    .catch(() => handleError(res, "Something wrong"));
+});
+
+// app.get(`/${COLLECTION}`, (req, res) => {});
+
+app.get(`/${COLLECTION}/:id`, (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection(COLLECTION)
+      .findOne({ _id: new ObjectId(req.params.id) }) // findOne для получения одного элемента. ObjectId специальная обертка из Mongo
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch(() => handleError(res, "Something wrong"));
+  } else {
+    handleError(res, "Wrong id");
+  }
+});
+
+app.delete(`/${COLLECTION}/:id`, (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection(COLLECTION)
+      .deleteOne({ _id: new ObjectId(req.params.id) }) // findOne для получения одного элемента. ObjectId специальная обертка из Mongo
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch(() => handleError(res, "Something wrong"));
+  } else {
+    handleError(res, "Wrong id");
+  }
 });
